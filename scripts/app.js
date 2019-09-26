04e1482e94f550c3b34e968676e29c2a5ad29fd4
grudinsky <g.rudzianskas@gmail.com>
2019-09-26 12:39:45 +0100

    Added playerpill mode that freezes ghosts after the pill is eaten by pacman. Ghosts colour change and ghost behaviour during pill mode still oustatnding

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const path = [21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 36, 37, 38, 41, 48, 51, 61, 81, 101, 121, 68, 71, 58, 78, 98, 88, 91, 82, 83,
     84, 85, 86, 87, 89, 90, 92, 93, 94, 95, 96, 97, 168, 168, 169, 170, 171, 44, 64, 104, 124, 144, 164, 184, 204, 224, 244, 264, 284, 304, 324, 321,
-    322, 323, 341, 361, 362, 363, 364, 365, 366, 367, 368, 369, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 378, 358, 338, 337, 336, 335,
+    322, 323, 341, 361, 362, 363, 364, 365, 366, 367, 368, 369, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 358, 338, 337, 336, 335,
     315, 295, 275, 255, 235, 215, 195, 175, 155, 135, 115, 136, 137, 138, 118, 200, 201, 202, 203, 205, 206, 207, 214, 213, 212, 216, 217, 218, 219,
     122, 123, 75, 55, 125, 126, 127, 128, 131, 132, 133, 134, 148, 151, 167, 187, 172, 192, 227, 232, 228, 229, 230, 231, 248, 251, 268, 271, 288, 291,
-    285, 286, 306, 326, 327, 308, 328, 311, 331, 332, 333, 313, 293, 294, 296, 297, 298, 318, 278, 258, 257, 256, 283, 282, 301, 281, 261, 241, 242, 243, 347, 352, 289, 290, 189, 190]
-  const food = [21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 36, 37, 38, 41, 48, 51, 61, 81, 101, 121, 68, 71, 58, 78, 98, 88, 91, 82, 83,
+    285, 286, 306, 326, 327, 308, 328, 311, 331, 332, 333, 313, 293, 294, 296, 297, 298, 318, 278, 258, 257, 256, 283, 282, 301, 281, 261, 241, 242, 243, 347, 352, 289, 290]
+  const food = [22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 36, 37, 41, 48, 51, 61, 81, 101, 121, 68, 71, 58, 78, 98, 88, 91, 82, 83,
     84, 85, 86, 87, 89, 90, 92, 93, 94, 95, 96, 97, 168, 168, 169, 170, 171, 44, 64, 104, 124, 144, 164, 184, 204, 224, 244, 264, 284, 304, 324, 321,
-    322, 323, 341, 361, 362, 363, 364, 365, 366, 367, 368, 369, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 378, 358, 338, 337, 336, 335,
+    322, 323, 341, 362, 363, 364, 365, 366, 367, 368, 369, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 358, 338, 337, 336, 335,
     315, 295, 275, 255, 235, 215, 195, 175, 155, 135, 115, 136, 137, 138, 118, 205, 206, 207, 214, 213, 212,
     122, 123, 75, 55, 125, 126, 127, 128, 131, 132, 133, 134, 148, 151, 167, 187, 172, 192, 227, 232, 228, 229, 230, 231, 248, 251, 268, 271, 288, 291,
     285, 286, 306, 326, 327, 308, 328, 311, 331, 332, 333, 313, 293, 294, 296, 297, 298, 318, 278, 258, 257, 256, 283, 282, 301, 281, 261, 241, 242, 243, 347, 352, 289, 290]
-
+  const pills = [21, 38, 361, 378]
   // Creating grid board 
   for (let i = 0; i < width ** 2; i++) {
     const cell = document.createElement('DIV')
@@ -40,10 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (food.includes(i)) {
       cell.classList.add('food')
     }
+    if (pills.includes(i)) {
+      cell.classList.add('pill')
+    }
   }
 
   // Defining player
-  let playerIdx = 378
+  let playerIdx = 89
   let playerLives = 3
   let playerPoints = 0
   let playerDead = false
@@ -61,58 +64,52 @@ document.addEventListener('DOMContentLoaded', () => {
   // const player = document.querySelector('.grid div.player')
   cells[playerIdx].classList.add('player')
 
-
-
   // Player move logic
 
   function playerMove() {
-    const playerId = setInterval(() => {
-      clearInterval(playerId)
-      document.addEventListener('keyup', (e) => {
-        if (playerLives > 0) {
-          cells[playerIdx].classList.remove('player')
-          switch (e.keyCode) {
-            case 37:
-              if (path.includes(playerIdx - 1)) {
-                playerIdx -= 1
-                playerPoints
-              } else if (playerX === 0) {
-                playerIdx += width - 1
-              }
-              break
-            case 38:
-              if (path.includes(playerIdx - width))
-                playerIdx -= width
-              break
-            case 39:
-              if (path.includes(playerIdx + 1)) {
-                playerIdx += 1
-              } else if (
-                playerX === width - 1) {
-                playerIdx -= width - 1
-              }
-              break
-            case 40:
-              if (path.includes(playerIdx + width))
-                playerIdx += width
-              break
-          }
-          if (cells[playerIdx].classList.contains('food')) {
-            cells[playerIdx].classList.remove('food')
-            //This only updates inside the switch statetment- NEEDS FIX
-            playerPoints += 10
-          }
+    document.addEventListener('keyup', (e) => {
 
-          playerCoordinates()
-          cells[playerIdx].classList.remove('food')
-          cells[playerIdx].classList.add('player')
-          // console.log(playerPoints)
-          // clearInterval(playerId)
-        }
-
-      })
-    }, 300)
+      if (playerLives === 0) return
+      cells[playerIdx].classList.remove('player')
+      switch (e.keyCode) {
+        case 37:
+          if (path.includes(playerIdx - 1)) {
+            playerIdx -= 1
+            playerPoints
+          } else if (playerX === 0) {
+            playerIdx += width - 1
+          }
+          break
+        case 38:
+          if (path.includes(playerIdx - width))
+            playerIdx -= width
+          break
+        case 39:
+          if (path.includes(playerIdx + 1)) {
+            playerIdx += 1
+          } else if (
+            playerX === width - 1) {
+            playerIdx -= width - 1
+          }
+          break
+        case 40:
+          if (path.includes(playerIdx + width))
+            playerIdx += width
+          break
+      }
+      if (cells[playerIdx].classList.contains('food')) {
+        cells[playerIdx].classList.remove('food')
+        playerPoints += 10
+      } else if (cells[playerIdx].classList.contains('pill')) {
+        cells[playerIdx].classList.remove('pill')
+        playerPoints += 100
+        playerPillMode()
+      }
+      playerCoordinates()
+      cells[playerIdx].classList.add('player')
+    })
   }
+
   playerMove()
 
   class Ghost {
@@ -165,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
       cells[this.idx].id = 'no-id'
 
       // ghostPrevStep = ghostIdx
-      
+
       const randomRoute = Math.floor(Math.random() * 2)
       // console.log(randomRoute, this.name)
 
@@ -185,22 +182,22 @@ document.addEventListener('DOMContentLoaded', () => {
         // console.log('HIGH LEFT')
         if (randomRoute === 0 && path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
           this.goDown()
-        } else if (randomRoute === 1 && path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
+        } else if (path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
           this.goRight()
-        } else if (randomRoute === 0 && path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
+        } else if (path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
           this.goLeft()
-        } else if (randomRoute === 1 && path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
+        } else if (path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
           this.goUp()
         }
         //if ghost is HIGH RIGHT
       } else if (this.x > playerX && this.y < playerY) {
         if (randomRoute === 0 && path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
           this.goLeft()
-        } else if (randomRoute === 1 && path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
+        } else if (path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
           this.goDown()
-        } else if (randomRoute === 0 && path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
+        } else if (path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
           this.goUp()
-        } else if (randomRoute === 1 && path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
+        } else if (path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
           this.goRight()
         }
         // ghost is BOTTOM RIGHT
@@ -208,11 +205,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // console.log('BOTTOM RIGHT')
         if (randomRoute === 0 && path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
           this.goUp()
-        } else if (randomRoute === 1 && path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
+        } else if (path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
           this.goLeft()
-        } else if (randomRoute === 0 && path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
+        } else if (path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
           this.goDown()
-        } else if (randomRoute === 1 && path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
+        } else if (path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
           this.goRight()
         } else {
           return
@@ -221,13 +218,13 @@ document.addEventListener('DOMContentLoaded', () => {
       } else if (this.x < playerX && this.y > playerY) {
         if (randomRoute === 0 && path.includes(this.idx + 1) && (this.prevStep !== (this.idx + 1))) {
           this.goRight()
-        } else if (randomRoute === 1 && path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
+        } else if (path.includes(this.idx - width) && (this.prevStep !== (this.idx - width))) {
           this.goUp()
-        } else if (randomRoute === 0 && path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
+        } else if (path.includes(this.idx + width) && (this.prevStep !== (this.idx + width))) {
           this.goDown()
-        } else if (randomRoute === 0 && path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
+        } else if (path.includes(this.idx - 1) && (this.prevStep !== (this.idx - 1))) {
           this.goLeft()
-        } 
+        }
         //Ghost higher or lower on same X axis
       } else if (this.x === playerX && this.y !== playerY) {
         if (path.includes(this.idx - width) && (this.y > playerY)) {
@@ -236,7 +233,7 @@ document.addEventListener('DOMContentLoaded', () => {
           this.goDown()
         } else if (randomRoute === 0 && path.includes(this.idx - 1)) {
           this.goLeft()
-        } else if (randomRoute === 1 && path.includes(this.idx + 1)) {
+        } else if (path.includes(this.idx + 1)) {
           this.goRight()
         }
         //Ghost is left or right on same Y axis
@@ -247,13 +244,13 @@ document.addEventListener('DOMContentLoaded', () => {
           this.goLeft()
         } else if (randomRoute === 0 && path.includes(this.idx + width)) {
           this.goDown()
-        } else if (randomRoute === 1 && path.includes(this.idx - width)) {
+        } else if (path.includes(this.idx - width)) {
           this.goUp()
         }
       } else if (this.x === playerX && this.y === playerY) {
         playerLives -= 1
         livesLeft.innerHTML = playerLives
-        playerDead = true   
+        playerDead = true
       }
 
       if (cells[this.idx].classList.contains('food')) {
@@ -272,8 +269,6 @@ document.addEventListener('DOMContentLoaded', () => {
         this.id = setInterval(() => {
           this.brain()
           if (playerDead === true) {
-            console.log(`clearing interval of: ${this.name}`)
-            // clearInterval(this.id)
             clearInterval(this.id)
             playerDead = false
             cells[this.idx].classList.remove('ghost')
@@ -286,28 +281,51 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   }
-  function gameOver() {
-    clearInterval(pinky.id)
-    clearInterval(winky.id)
-    clearInterval(stinky.id)
-    clearInterval(blinky.id)
+  //GHOST object class ends
+  function clearIntervalAll() {
+    clearInterval(firstGhost.id)
+    clearInterval(secondGhost.id)
+    clearInterval(thirdGhost.id)
+    clearInterval(fourthGhost.id)
+  }
 
+  function gameOver() {
+    clearIntervalAll()
     const gameOverScreen = document.createElement('DIV')
     gameOverScreen.textContent = 'GAME OVER!'
     gameOverScreen.classList.add('game-over')
     document.body.appendChild(gameOverScreen)
   }
 
+  function playerPillMode() {
+    // duration of pill mode set as a value of pillModeTimer variable
+    clearIntervalAll()
+    let pillModeTimer = 6
+    const pillTimerId = setInterval(() => {
+      pillModeTimer -= 1
+      console.log(pillModeTimer)
+      if (pillModeTimer === 0) {
+        clearInterval(pillTimerId)
+        deployGhosts()
+      }
+    }, 1000)
+    if (pillModeTimer === 0) {
+      console.log('exiting')
+    }
+  }
 
-  const pinky = new Ghost('pinky', 227)
-  const winky = new Ghost('winky', 232)
-  const stinky = new Ghost('stinky', 167)
-  const blinky = new Ghost('blinky', 172)
+  const firstGhost = new Ghost('pinky', 227)
+  const secondGhost = new Ghost('winky', 232)
+  const thirdGhost = new Ghost('stinky', 167)
+  const fourthGhost = new Ghost('blinky', 172)
 
   playerPoints
-  pinky.move()
-  winky.move()
-  blinky.move()
-  stinky.move()
+  function deployGhosts() {
+    firstGhost.move()
+    secondGhost.move()
+    thirdGhost.move()
+    fourthGhost.move()
+  }
+  deployGhosts()
 
 })
